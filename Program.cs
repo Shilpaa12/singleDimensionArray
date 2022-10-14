a43fe21c7e709dcc94f857f236ddb633d5ce7df7
Shilpaa12 <shilpapathlavath7@gmail.com>
2022-10-14 11:23:21 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int aSize;
+Console.WriteLine("Enter the Array Size :");
+aSize =Convert.ToInt32( Console.ReadLine());
+
+int[] arrayData = new int[aSize];
+
+Console.WriteLine("Enter the Array Elements :");
+for (int i = 0; i < aSize; i++)
+{
+    arrayData[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+
+Console.WriteLine("The Array Elements :");
+for (int i = 0; i < aSize; i++)
+{
+    Console.WriteLine(arrayData[i]);
+}
+
+
+
